e28303ec3ef71cb4380ad121c61c314618d50068
msbaq <msbaqir@gmail.com>
2021-02-26 16:25:06 -0500

    edit2

--- assignment2/Controllers/J3Controller.cs
+++ assignment2/Controllers/J3Controller.cs
@@ -10,7 +10,7 @@ namespace assignment2.Controllers
     [Route("api/J3/{a}")]
     public class J3Controller : ApiController
     {
-        public Stack<int> GetNumbersIn(int a)
+        public string GetNumbersIn(int[] a)
         {//J3 problem: https://cemc.math.uwaterloo.ca/contests/computing/2021/ccc/juniorEF.pdf
          //Each instruction is a sequence of five digits which represents a direction to turn and the
          //number of steps to take.
@@ -21,16 +21,23 @@ namespace assignment2.Controllers
          //The remaining three digits represent the number of steps to take which will always be at
          //least 100.
          /*Since the user input is a sequence of 5 digits, we need to first separate
-         * the 5 digits. to do that i borrowed this code from https://stackoverflow.com/questions/829174/is-there-an-easy-way-to-turn-an-int-into-an-array-of-ints-of-each-digit
-         * i don't fully know how it works and i havent been able to get it to work completely. but this method
-         * was able to return a stack of separated integers.*/
-           if (a == 0) return new Stack<int>();
-           var numbers = GetNumbersIn(a / 10);
-
-           numbers.Push(a % 10);
-         //since i am not familiar with stacks i converted the stack values into an array
-           int[] m = numbers.ToArray();
-         //i then assigned new variables to the integers in the array so it is easier to work with
+         * the 5 digits. the input thus comes in the form of an integer array. then use two loops to extract
+         individual integers from that array.*/
+           int i = 0;
+           while (a[i] == 99999 ){
+                //this loop would separate the individual items in the array
+                //it will stop when the value in the array equals 99999
+                //but i am not sure how I would code for the rest
+                //the output for the loop would be a list or stack perhaps
+                //which could then be used to access individual numbers to then extract digits
+           }
+           for (i = 0; i = 5; i++)
+            {
+                //this loop would then separate the 5 individual digits in the integers
+                //extracted from the first loop
+            }
+           //digits extracted from the FOR loop would be added into another array
+           //i would then create int variables to assign those digits to them and make it easier to work with
            int v = m[0];
            int w = m[1];
            int x = m[2];
@@ -57,9 +64,7 @@ namespace assignment2.Controllers
                return "You have entered incorrect values";
            }
            /*once all this was sorted, it would have been easy to just return the string and integer values
-            * to show direction and number of steps to take. But my issue is the Stack class now which is
-            not able to a return string type. Once this code had started to work we can scale it up to other integer inputs
-           to get the whole set of instructions*/
+            * to show direction and number of steps to take. */
            return signal + x.ToString() + y.ToString() + z.ToString();
 
         }
